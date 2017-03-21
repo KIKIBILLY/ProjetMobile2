498515b89d9a886dd767699c34b9ff7e10180473
KIKIBILLY <da_bourihane@esi.dz>
2017-03-21 19:50:59 +0100

    ok

---
+++ app/src/main/java/com/example/user/projetmobile/LogementAdapter.java
@@ -0,0 +1,32 @@
+package com.example.user.projetmobile;
+
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+
+/**
+ * Created by User on 21/03/2017.
+ */
+
+public class LogementAdapter extends BaseAdapter {
+
+    @Override
+    public int getCount() {
+        return 0;
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        return null;
+    }
+}
---
+++ app/src/main/java/com/example/user/projetmobile/Logements.java
@@ -0,0 +1,11 @@
+package com.example.user.projetmobile;
+
+import java.io.Serializable;
+
+/**
+ * Created by User on 21/03/2017.
+ */
+
+public class Logements implements Serializable {
+
+}
