fc588f4435a2cf65a45e362a80d3db5642c3750f
KIKIBILLY <da_bourihane@esi.dz>
2017-03-21 20:45:49 +0100

    ok

--- app/src/main/java/com/example/user/projetmobile/MainActivity.java
+++ app/src/main/java/com/example/user/projetmobile/MainActivity.java
@@ -13,6 +13,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
@@ -41,6 +42,16 @@ public class MainActivity extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+
+        Button bt1=(Button) findViewById(R.id.button);
+        bt1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(MainActivity.this,ListeAnonces.class);
+                startActivity(intent);
+            }
+        });
+
     }
 
     @Override
