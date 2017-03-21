bf59f0bb609aba8f6c93ed106384862b9b43a9db
KIKIBILLY <da_bourihane@esi.dz>
2017-03-21 19:22:20 +0100

    Initial commit

---
+++ app/src/main/java/com/example/user/projetmobile/DetailAnonces.java
@@ -0,0 +1,13 @@
+package com.example.user.projetmobile;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DetailAnonces extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail_anonces);
+    }
+}
