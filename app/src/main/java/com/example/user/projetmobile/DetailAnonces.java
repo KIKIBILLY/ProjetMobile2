d5860a34e1c739b848d8d6ea6b927bd5bb3b6422
KIKIBILLY <da_bourihane@esi.dz>
2017-03-21 23:25:47 +0100

    ok

--- app/src/main/java/com/example/user/projetmobile/DetailAnonces.java
+++ app/src/main/java/com/example/user/projetmobile/DetailAnonces.java
@@ -1,7 +1,10 @@
 package com.example.user.projetmobile;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class DetailAnonces extends AppCompatActivity {
 
@@ -9,5 +12,19 @@ public class DetailAnonces extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail_anonces);
+        Intent intent=getIntent();
+        Logements logements=(Logements) intent.getSerializableExtra("logement");
+
+        ImageView image=(ImageView) findViewById(R.id.imageView4);
+        TextView prix=(TextView) findViewById(R.id.textView5);
+        TextView nbchambre=(TextView) findViewById(R.id.textView6);
+        TextView surface=(TextView) findViewById(R.id.textView7);
+
+
+        image.setImageResource(logements.getId_image());
+        prix.setText(logements.getPrix());
+        nbchambre.setText(logements.getNombre_chambre());
+        surface.setText(logements.getSurface());
+
     }
 }
